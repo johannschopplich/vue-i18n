42d15f94cdbf0653f5769d0fae232fb60d12fc69
Johann Schopplich <mail@johannschopplich.com>
2023-09-12 16:47:05 +0200

    feat: `LocaleMessages` type

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,47 @@
 import type { App, ComputedRef } from 'vue'
 
-export type LocaleMessages = Record<string, Record<string, any>>
+export type LocaleMessageValue<Message = string> =
+  | LocaleMessageDictionary<any, Message>
+  | string
 
-export interface I18nConfig {
-  defaultLocale?: string
-  locales?: string[]
-  messages?: LocaleMessages
+export declare type LocaleMessageType<T, Message = string> = T extends string
+  ? string
+  : T extends () => Promise<infer P>
+    ? LocaleMessageDictionary<P, Message>
+    : T extends (...args: infer Arguments) => any
+      ? (...args: Arguments) => ReturnType<T>
+      : T extends Record<string, unknown>
+        ? LocaleMessageDictionary<T, Message>
+        : T extends Array<T>
+          ? { [K in keyof T]: T[K] }
+          : T
+
+export type LocaleMessageDictionary<T, Message = string> = {
+  [K in keyof T]: LocaleMessageType<T[K], Message>;
+}
+
+export type LocaleMessage<Message = string> = Record<
+  string,
+  LocaleMessageValue<Message>
+>
+
+export type LocaleMessages<Locale extends string> = Record<Locale, LocaleMessage>
+
+export interface I18nConfig<Locale extends string = string> {
+  defaultLocale?: Locale
+  locales?: Locale[]
+  messages?: LocaleMessages<Locale>
   /** @default 'warn' */
   logLevel?: 'warn' | 'silent'
 }
 
-export interface I18nInstance {
-  defaultLocale: string
-  locale: ComputedRef<string>
-  locales: readonly string[]
-  messages: LocaleMessages
+export interface I18nInstance<Locale extends string = string> {
+  defaultLocale: Locale
+  locale: ComputedRef<Locale>
+  locales: readonly Locale[]
+  messages: LocaleMessages<Locale>
   t: (key: string, params?: Record<string, any>) => string
-  setLocale: (locale: string) => void
+  setLocale: (locale: Locale) => void
   getLocale: () => string
   install(app: App): void
 }
